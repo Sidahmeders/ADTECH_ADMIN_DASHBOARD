48b42503bec7edab7cbdf5dbababb914fedae696
sidahmed <ahorob57@gmail.com>
2021-05-27 04:09:59 +0100

    finish styling the datepicker components

--- src/components/common/form/FileInputElement/index.js
+++ src/components/common/form/FileInputElement/index.js
@@ -1,10 +1,40 @@
+import { useRef, useState } from 'react'
 import './style.scss'
 
 export default function FileInputElement({ label }) {
+    const FileInputRef = useRef()
+
+    const focusInput = () => {
+        FileInputRef.current.click()
+    }
+
+    const [userFiles, setUserFiles] = useState({
+        file: '',
+        tempFile: ''
+    })
+
+    const onFileChange = (event) => {
+        setUserFiles({
+            file: event.target.files[0],
+            tempFile: URL.createObjectURL(event.target.files[0])
+        })
+    }
+
+    console.log(userFiles)
+
     return (
         <div className="file-input-element">
-            <label htmlFor={label}>{label}</label>
-            <input type="file" id={label} />
+            <label>{label}</label>
+            <input
+                type="file"
+                onChange={onFileChange}
+                ref={FileInputRef}
+                style={{ display: 'none' }}
+            />
+            <p className="file-picker" onClick={focusInput}>
+                click to select an image
+            </p>
+            <img width="300px" className="image-preview" src={userFiles.tempFile} alt="myfile" />
         </div>
     )
 }
