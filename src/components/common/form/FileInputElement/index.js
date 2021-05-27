39ac217a5f428c1a3dab016dd0dcdc048d950610
sidahmed <ahorob57@gmail.com>
2021-05-27 04:18:58 +0100

    finish styling the datepicker element

--- src/components/common/form/FileInputElement/index.js
+++ src/components/common/form/FileInputElement/index.js
@@ -3,38 +3,26 @@ import './style.scss'
 
 export default function FileInputElement({ label }) {
     const FileInputRef = useRef()
+    const focusInput = () => FileInputRef.current.click()
 
-    const focusInput = () => {
-        FileInputRef.current.click()
-    }
-
-    const [userFiles, setUserFiles] = useState({
-        file: '',
-        tempFile: ''
-    })
-
+    const [file, setFile] = useState('')
     const onFileChange = (event) => {
-        setUserFiles({
-            file: event.target.files[0],
-            tempFile: URL.createObjectURL(event.target.files[0])
-        })
+        setFile(() => URL.createObjectURL(event.target.files[0]))
     }
-
-    console.log(userFiles)
 
     return (
         <div className="file-input-element">
             <label>{label}</label>
             <input
                 type="file"
-                onChange={onFileChange}
                 ref={FileInputRef}
+                onChange={onFileChange}
                 style={{ display: 'none' }}
             />
             <p className="file-picker" onClick={focusInput}>
                 click to select an image
             </p>
-            <img width="300px" className="image-preview" src={userFiles.tempFile} alt="myfile" />
+            <img width="300px" className="image-preview" src={file} alt="file-picker" />
         </div>
     )
 }
