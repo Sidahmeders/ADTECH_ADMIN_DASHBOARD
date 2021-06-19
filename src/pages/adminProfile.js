ef6669fd9e44806b0c1702380445a1b187c88ac2
sidahmed <ahorob57@gmail.com>
2021-06-19 11:04:20 +0100

    install some new depenedcies

--- src/pages/adminProfile.js
+++ src/pages/adminProfile.js
@@ -11,14 +11,18 @@ export default function AdminProfile() {
 
     return (
         <div className="admin-profile">
-            <div className="card">
-                <p>full name: {adminUserInfo.first_name + ' ' + adminUserInfo.last_name}</p>
-                <p>birth date: {adminUserInfo.birth_date.split('T')[0]}</p>
-                <p>faculty: {adminUserInfo.faculty}</p>
-                <p>specialty: {adminUserInfo.specialty}</p>
-                <p>phone_number: {adminUserInfo.phone_number}</p>
-                <p>email: {adminUserInfo.email}</p>
-            </div>
+            {adminUserInfo ? (
+                <div className="card">
+                    <p>full name: {adminUserInfo.first_name + ' ' + adminUserInfo.last_name}</p>
+                    <p>birth date: {adminUserInfo.birth_date.split('T')[0]}</p>
+                    <p>faculty: {adminUserInfo.faculty}</p>
+                    <p>specialty: {adminUserInfo.specialty}</p>
+                    <p>phone_number: {adminUserInfo.phone_number}</p>
+                    <p>email: {adminUserInfo.email}</p>
+                </div>
+            ) : (
+                ''
+            )}
         </div>
     )
 }
