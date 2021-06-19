554a0e33e2b133d26a3e43ccf8bdf226ee0bec64
sidahmed <ahorob57@gmail.com>
2021-06-19 05:06:07 +0100

    added the admin scss file

--- src/pages/adminProfile.js
+++ src/pages/adminProfile.js
@@ -1,3 +1,5 @@
+import '../styles/adminProfile.scss'
+
 function getAdminUserFromLocalStorage() {
     let adminUserInfo = localStorage.getItem('adminUserInfo')
     adminUserInfo = JSON.parse(adminUserInfo)
@@ -6,11 +8,17 @@ function getAdminUserFromLocalStorage() {
 
 export default function AdminProfile() {
     const adminUserInfo = getAdminUserFromLocalStorage()
-    console.log(adminUserInfo)
 
     return (
-        <div>
-            <h2>Admin Profile Page</h2>
+        <div className="admin-profile">
+            <div className="card">
+                <p>full name: {adminUserInfo.first_name + ' ' + adminUserInfo.last_name}</p>
+                <p>birth date: {adminUserInfo.birth_date.split('T')[0]}</p>
+                <p>faculty: {adminUserInfo.faculty}</p>
+                <p>specialty: {adminUserInfo.specialty}</p>
+                <p>phone_number: {adminUserInfo.phone_number}</p>
+                <p>email: {adminUserInfo.email}</p>
+            </div>
         </div>
     )
 }
