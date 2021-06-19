5ac977e1a27b7b4ef19a1a41233c8641b33f6a00
sidahmed <ahorob57@gmail.com>
2021-06-19 10:40:14 +0100

    added the service worker files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+import * as serviceWorker from './serviceWorkerRegistration'
 import { ContextProvider } from './context'
 
 ReactDOM.render(
