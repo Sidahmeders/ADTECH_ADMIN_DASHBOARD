ad0cd08afd1f4b17ac13aab9c18e3140280b1ea3
sidahmed boutaraa <ahorob57@gmail.com>
2021-08-15 18:59:16 +0200

    completed some basic functionaty of the resultTab component

--- src/components/dentalCharts/cephalo/resultTab/ResultTab.js
+++ src/components/dentalCharts/cephalo/resultTab/ResultTab.js
@@ -4,11 +4,34 @@ import { ContextConsumer } from '../../../../context'
 
 export default function ResultTab() {
     const { cephaloResult } = useContext(ContextConsumer)
-    console.info(cephaloResult, 'CEPHALO RESULT')
+    const { angles, distances } = cephaloResult
+
+    console.log(angles, 'ANG')
+    console.log(distances, 'DIS')
+
+    const renderResult = (object) => {
+        const arr = []
+        for (let entry in object) {
+            arr.push(`${entry}-${object[entry]}`)
+        }
+        return arr.map((item) => {
+            const res = item.split('-')
+            return (
+                <>
+                    <p>
+                        {res[0]}: <span>{res[1]}</span>
+                    </p>
+                </>
+            )
+        })
+    }
 
     return (
         <div className="result-tab">
-            <h1>HELLO CEPHALO RESULT</h1>
+            <h3>angels</h3>
+            {renderResult(angles)}
+            <h3>distances</h3>
+            {renderResult(distances)}
         </div>
     )
 }
