79a63f89ac005e8fe36502c5c77e1e3ebcc895a2
Sidahmeders <ahorob57@gmail.com>
2021-08-30 23:40:29 +0200

    created the cephalo interpertations method

--- src/components/dentalCharts/cephalo/resultTab/ResultTab.js
+++ src/components/dentalCharts/cephalo/resultTab/ResultTab.js
@@ -1,13 +1,25 @@
 import './style.scss'
 import { useContext } from 'react'
 import { ContextConsumer } from '../../../../context'
+import interpertations from './interpertations.js'
+
+function getInterpertations(angleValue, targetAngle) {
+    angleValue = parseInt(angleValue)
+    console.log(angleValue)
+    if (!angleValue || !targetAngle) {
+        return '<-------------------------->'
+    } else if (targetAngle.min >= angleValue && angleValue <= targetAngle.max) {
+        return targetAngle.average
+    } else if (targetAngle.max >= angleValue) {
+        return targetAngle.high
+    } else if (targetAngle.min <= angleValue) {
+        return targetAngle.low
+    }
+}
 
 export default function ResultTab() {
     const { cephaloResult } = useContext(ContextConsumer)
     const { angles, distances } = cephaloResult
-
-    console.log(angles, 'ANG')
-    console.log(distances, 'DIS')
 
     const renderResult = (object) => {
         const arr = []
@@ -16,10 +28,18 @@ export default function ResultTab() {
         }
         return arr.map((item) => {
             const res = item.split('-')
+            const angleName = res[0]
+            const angleValue = res[1]
+
+            const targetAngle = interpertations[angleName]
+            const angleInterpertation = getInterpertations(angleValue, targetAngle)
+
             return (
                 <>
                     <p>
-                        {res[0]}: <span>{res[1]}</span>
+                        <span className="name">{angleName}:</span>
+                        <span className="value">{angleValue}°</span>
+                        <span className="interpretation">{angleInterpertation}</span>
                     </p>
                 </>
             )
---
+++ src/components/dentalCharts/cephalo/resultTab/interpertations.js
@@ -0,0 +1,9 @@
+export default {
+    ANB: {
+        min: 78.5,
+        max: 82.5,
+        average: 'Classe I squelettique selon BALLARD', // 78.5 >= Angle =< 82.5
+        high: 'Classe II squelettique selon BALLARD', // Angle > 82.5
+        low: 'Classe III squelettique selon BALLARD' // Angle < 78.5
+    }
+}
